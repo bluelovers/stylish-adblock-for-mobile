4de5e02749128134e5343777ea975abc924f52c9
bluelovers <codelovers@users.sourceforge.net>
2017-04-11 08:14:52 +0800

    content-farm (Step: 4)
    Step: 4
    
    [A ] new file:
    
    	src/site/thenewslens.com.scss
    
    [ M] modified - Changes not staged for commit:
    
    	dist/stylish.adblock.mobile.css
    	dist/stylish.adblock.mobile.css.map
    	script/build-site.js
    	src/_include_site.scss
    	src/lib/lib.scss
    	src/stylish.adblock.mobile.scss

--- script/build-site.js
+++ script/build-site.js
@@ -12,18 +12,19 @@ const target_file = path.resolve(__dirname, dir, '_include_site.scss');
 
 module.exports = (cb) =>
 {
-	return glob("site/*.scss", {
+	return glob("site/**/*.scss", {
 		cwd: path.resolve(__dirname, dir),
 	}, function (err, files) {
 
 		let data = '';
 
 		let sites = [];
+		let content_farm = [];
 
 		for (let file of files)
 		{
 			//let domain = path.basename(file, '.scss');
-			let filename = path.basename(file);
+			let filename = path.basename(file).replace(/[\\\/]/g, '_');
 			let domain = path.basename(filename, '.scss');
 
 			data += `/* site: ${domain} */\n`;
@@ -35,6 +36,11 @@ module.exports = (cb) =>
 			else
 			{
 				sites.push(domain);
+			}
+
+			if (/content_farm/.test(file))
+			{
+				content_farm.push(domain);
 			}
 
 			/*
@@ -60,7 +66,26 @@ module.exports = (cb) =>
 \n`;
 		}
 
-		data = `// this file is auto build, don't edit this\n/* sites: ${sites.join(", ")} */\n${data}`;
+		data = `// this file is auto build, don't edit this\n/* sites: ${sites.join(", ")} ... and others.*/\n${data}`;
+
+
+		content_farm = content_farm.map((v) =>
+		{
+			return `domain("${v}")`;
+		}, '').join(', ');
+
+		fs.writeFile(path.resolve(__dirname, dir, '_include_site_content_farm.scss'), `
+/* content_farm */
+\$domain_content_farm: 'content_farm';
+\$domain: 'content_farm';
+@-moz-document ${content_farm}
+{
+	@include content-farm-include($domain);
+}
+`, function (err)
+		{
+
+		});
 
 		fs.writeFile(target_file, data, function (err)
 		{
