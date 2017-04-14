55de030e7ce266c38714d819a9ff84ca2547fdb1
bluelovers <codelovers@users.sourceforge.net>
2017-04-15 05:26:46 +0800

    newinclide (Step: 19)
    Step: 19
    
    [A ] new file:
    
    	src/_include/_alldomain.scss
    	src/_include/_include_art.scss
    	src/_include/_include_blog.scss
    	src/_include/_include_comic.scss
    	src/_include/_include_content_farm.scss
    	src/_include/_include_mobile.scss
    	src/_include/_include_news.scss
    	src/_include/_include_yahoo.scss
    	src/include/_include_art.scss
    	src/include/_include_blog.scss
    	src/include/_include_comic.scss
    	src/include/_include_content_farm.scss
    	src/include/_include_mobile.scss
    	src/include/_include_news.scss
    	src/include/_include_yahoo.scss
    
    [D ] deleted:
    
    	src/include/content-farm.scss
    
    [ M] modified - Changes not staged for commit:
    
    	dist/stylish.adblock.mobile.css
    	dist/stylish.adblock.mobile.css.map
    	script/build-site.js
    	src/_include_site.scss
    	src/_include_site_content_farm.scss
    	src/lib/lib.scss
    	src/site/content_farm/cocomy.net.scss
    	src/site/content_farm/ispot.news.scss
    	src/site/content_farm/life.tw.scss
    	src/site/content_farm/pixpo.net.scss
    	src/site/content_farm/twgreatdaily.com.scss
    	src/stylish.adblock.mobile.scss

--- script/build-site.js
+++ script/build-site.js
@@ -10,10 +10,12 @@ const dir = '../src/';
 
 const target_file = path.resolve(__dirname, dir, '_include_site.scss');
 
+const cwd = path.resolve(__dirname, dir);
+
 module.exports = (cb) =>
 {
 	return glob("site/**/*.scss", {
-		cwd: path.resolve(__dirname, dir),
+		cwd: cwd,
 	}, function (err, files) {
 
 		let data = '';
@@ -21,12 +23,23 @@ module.exports = (cb) =>
 		let sites = [];
 		let content_farm = [];
 
+		const dirkey_all = '_alldomain';
+
+		let scache = {};
+		scache[dirkey_all] = [];
+
 		for (let file of files)
 		{
 			//let domain = path.basename(file, '.scss');
 			let filename = path.basename(file).replace(/[\\\/]/g, '_');
 			let domain = path.basename(filename, '.scss');
 
+			let dirkey = path.dirname(file);
+			dirkey = dirkey.replace(/^site(\/|$)/, '').replace(/[\-\s]+/g, '_');
+
+			//dirkey = dirkey || '_';
+
+			data += `/* dirkey: ${dirkey} */\n`;
 			data += `/* site: ${domain} */\n`;
 
 			if (domain == '_all')
@@ -36,6 +49,22 @@ module.exports = (cb) =>
 			else
 			{
 				sites.push(domain);
+			}
+
+			(scache[dirkey_all] = scache[dirkey_all] || [], scache[dirkey_all]).push(domain);
+
+			if (dirkey)
+			{
+				dirkey = '_include_' + dirkey;
+
+				let a = dirkey.replace(/([_]+)/g, '$1').split(/[\\\/]/);
+
+				if (a.length > 1)
+				{
+					(scache[dirkey] = scache[dirkey] || [], scache[dirkey]).push(domain);
+				}
+
+				(scache[a[0]] = scache[a[0]] || [], scache[a[0]]).push(domain);
 			}
 
 			if (/content_farm/.test(file))
@@ -66,26 +95,62 @@ module.exports = (cb) =>
 \n`;
 		}
 
-		data = `// this file is auto build, don't edit this\n/* sites: ${sites.join(", ")} ... and others.*/\n${data}`;
+		console.log(scache);
 
+		let data2 = '';
 
-		content_farm = content_farm.map((v) =>
+		for (let dirkey in scache)
 		{
-			return `domain("${v}")`;
-		}, '').join(', ');
+			let domains = scache[dirkey];
 
-		fs.writeFile(path.resolve(__dirname, dir, '_include_site_content_farm.scss'), `
-/* content_farm */
-\$domain_content_farm: 'content_farm';
-\$domain: 'content_farm';
-@-moz-document ${content_farm}
+			domains = domains.map((v) =>
+			{
+				return `domain("${v}")`;
+			}, '').join(', ');
+
+			let file = path.resolve(cwd, '_include', `${dirkey}.scss`);
+
+			let _data = `/* ${dirkey} */\n\$domain_${dirkey.replace(/^_+/g, '')}: '${dirkey}';\n\$domain: '${dirkey}';
+@-moz-document ${domains}
 {
-	@include content-farm-include($domain);
-}
-`, function (err)
-		{
+	@include site-init-here($domain, $domain_${dirkey.replace(/^_+/g, '')});
+	
+	@import "../include/${dirkey}.scss";
+}`;
 
-		});
+			data2 += `\$domain: '${dirkey}';
+@import "_include/${dirkey}.scss";
+\n`;
+
+			fs.writeFileSync(file, _data);
+
+			let file2 = path.resolve(cwd, 'include', `${dirkey}.scss`);
+
+			if (!fs.existsSync(file2))
+			{
+				fs.writeFileSync(file2, '');
+			}
+		}
+
+		data = `// this file is auto build, don't edit this\n/* sites: ${sites.join(", ")} ... and others.*/\n${data2}\n${data}`;
+
+//		content_farm = content_farm.map((v) =>
+//		{
+//			return `domain("${v}")`;
+//		}, '').join(', ');
+//
+//		fs.writeFile(path.resolve(cwd, '_include_site_content_farm.scss'), `
+///* content_farm */
+//\$domain_include_content_farm: 'content_farm';
+//\$domain: 'content_farm';
+//@-moz-document ${content_farm}
+//{
+//	@include content-farm-include($domain);
+//}
+//`, function (err)
+//		{
+//
+//		});
 
 		fs.writeFile(target_file, data, function (err)
 		{
