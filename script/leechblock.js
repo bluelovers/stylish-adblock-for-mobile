5a4bb573d48ceff4414805634429040d10dbee1a
bluelovers <codelovers@users.sourceforge.net>
2017-05-08 10:20:28 +0800

    . (Step: 9)
    Step: 9
    
    [A ] new file:
    
    	dist/adaway.hosts.txt
    	dist/adblock.ini
    	dist/adblockpopups/adblockpopups.ini
    	dist/leechblock.list.txt
    	dist/noscript.untrusted.txt
    	script/adaway.gulp.task.js
    	script/adblock.gulp.task.js
    	script/adblockpopups.gulp.task.js
    	script/leechblock.js
    	script/lib/adblock.js
    	script/lib/common.js
    	script/noscript.untrusted.gulp.task.js
    	"src/adblock/LazyList SC.ini"
    	src/adblock/adblock-sc.ini
    	src/adblock/badware/惡意網站.txt
    	src/adblock/list/dmm.txt
    	src/adblock/whitelist/whitelist.txt
    
    [ M] modified - Changes not staged for commit:
    
    	gulpfile.js
    	src/leechblock/leechblock.list.txt

---
+++ script/leechblock.js
@@ -0,0 +1,44 @@
+/**
+ * Created by user on 2017/5/8.
+ */
+
+'use strict';
+
+const fs = require('fs');
+const glob = require('glob');
+const path = require('path');
+const Promise = require("bluebird");
+
+const LF = "\n";
+
+const dir = '../src/';
+
+const src_dir = path.resolve(__dirname, dir);
+const dist_dir = path.resolve(__dirname, '../dist');
+const cwd = path.resolve(__dirname, dir);
+
+module.exports = (done) =>
+{
+	let domains = fs.readFileSync(path.resolve(src_dir, 'leechblock/leechblock.list.txt'));
+
+	domains = array_unique(domains.toString().split(/\r\n|\r|\n/g));
+
+	domains = domains
+		.map((v) => v.toString().trim())
+		.filter((el, i, arr) => el)
+	;
+
+	domains.sort();
+
+	fs.writeFileSync(path.resolve(dist_dir, 'leechblock.list.txt'), domains.join(LF));
+
+	done();
+};
+
+/**
+ * @see http://www.jstips.co/zh_tw/javascript/deduplicate-an-array/
+ */
+function array_unique(arr)
+{
+	return Array.from(new Set(arr));
+}
