d8ef2a54c45b10878e6926b69d1c51abda2b2eb6
bluelovers <codelovers@users.sourceforge.net>
2017-05-09 15:35:17 +0800

    . (Step: 13)
    Step: 13
    
    [A ] new file:
    
    	src/_include/_include_acg.scss
    	src/adblock/list/yahoo.txt
    	src/include/_include_acg.scss
    	src/site/acg/4gamers.com.tw.scss
    	src/site/content_farm/gjoyz.co.scss
    	src/site/yahoo/news.yahoo.com.scss
    
    [ M] modified - Changes not staged for commit:
    
    	dist/adblock.ini
    	dist/stylish.adblock.mobile.css
    	dist/stylish.adblock.mobile.css.map
    	dist/stylish.facebook.css
    	dist/stylish.facebook.css.map
    	script/adblock.gulp.task.js
    	script/lib/adblock.js
    	src/_include/_alldomain.scss
    	src/_include/_include_content_farm.scss
    	src/_include/_include_yahoo.scss
    	src/_include_all.scss
    	src/_include_site.scss
    	src/lib/lib.scss
    	src/site/content_farm/teepr.com.scss
    	src/site/news/storm.mg.scss
    	src/site/userstyles.org.scss
    	src/stylish.facebook.scss

--- script/lib/adblock.js
+++ script/lib/adblock.js
@@ -6,7 +6,7 @@ class AdblockParser
 {
 	constructor(source)
 	{
-		let r = /^(?:\|+)?([^#\^\?\/\$]*)/;
+		let r = /^(?:[\|@]+)?(?:https?:\/\/)?([^#\^\?\/\$\|@]*)/;
 
 		let data = (Array.isArray(source) ? source : source.toString().split(/\r\n|\r|\n/g))
 			.reduce((a, b) =>
@@ -20,15 +20,15 @@ class AdblockParser
 						case '[':
 						case '!':
 							break;
-						case '@':
-							a[line[0]] = a[line[0]] || [];
-
-							if (a[line[0]].indexOf(line) == -1)
-							{
-								a[line[0]].push(line);
-							}
-
-							break;
+//						case '@':
+//							a[line[0]] = a[line[0]] || [];
+//
+//							if (a[line[0]].indexOf(line) == -1)
+//							{
+//								a[line[0]].push(line);
+//							}
+//
+//							break;
 						default:
 							let m = line.match(r);
 
@@ -54,25 +54,112 @@ class AdblockParser
 			'': [],
 		};
 
-		Object.keys(data)
+		let data3 = Object.keys(data)
+			.sort()
+			.reduce(function (a, v)
+			{
+				let m = v.match(/\.?([^\.]+(?:\.(?:co|com|org|net|edu|gov|mil|nom|[^\.\d]{1,2}))?\.[^\.]+)$/);
+
+				//console.log(v, m);
+
+				if (m && m[1])
+				{
+					a[m[1]] = a[m[1]] || [];
+					a[m[1]][m[1]] = a[m[1]][m[1]] || [];
+					a[m[1]][v] = a[m[1]][v] || [];
+
+					a[m[1]][v] = data[v].sort();
+				}
+				else
+				{
+					a[v] = a[v] || [];
+
+					a[v] = data[v].sort();
+				}
+
+				return a;
+			}, {});
+
+		//console.log('data3', data3);
+
+		Object.keys(data3)
 			.sort()
 			.forEach(function (v, i)
 			{
-				data2[v] = data[v];
+				if (Object.keys(data3[v]).length)
+				{
+					data2[v] = data3[v];
+				}
 			});
+
+		//console.log('data2', data2);
 
 		//console.log(data2);
 		this.data = data2;
 	}
 
-	toArray()
+	toArray(mode)
 	{
-		return Object.keys(this.data)
-			.reduce((a, b) => {
 
-				if (this.data[b].length)
+		let fn = (a, domain) =>
+		{
+			let b = [];
+
+			for (let k in a)
+			{
+				let v = a[k];
+
+				if (Array.isArray(v))
 				{
-					a = a.concat([`!\n! [${b}]\n!`], this.data[b]);
+					//console.log(444, domain, k, (k + 0) != (k + 0));
+
+					if (domain && (k != domain))
+					{
+						b.push(`!\n! [${k}]\n!`);
+					}
+
+					b = b.concat(fn(v, domain))
+				}
+				else
+				{
+					if (v[0] == '@')
+					{
+						b.unshift(v);
+					}
+					else
+					{
+						b.push(v);
+					}
+				}
+			}
+
+			return b;
+		};
+
+		return Object.keys(this.data)
+			.reduce((a, b) =>
+			{
+
+				if (Object.keys(this.data[b]).length)
+				{
+					//console.log(777, b);
+
+					let d = '';
+
+					if (Object.keys(this.data[b]).length != this.data[b].length)
+					{
+						let a = Object.keys(this.data[b]);
+						//a.shift();
+
+						if (a.length > 1)
+						{
+							d = ' Sites: ' + a.join(', ') + "\n!";
+
+							d = d.replace(/\s+$/g, '');
+						}
+					}
+
+					a = a.concat([`!\n! [${b}] =============>\n!${d}`], fn(this.data[b], b), [`!\n! ---------------------------------`]);
 				}
 
 				return a;
@@ -82,8 +169,7 @@ class AdblockParser
 
 	toString()
 	{
-		return this.toArray().join("\n");
-		;
+		return this.toArray().join("\n").replace(/(\!\n){2,}/g, "$1");
 	}
 
 	static load(source)
