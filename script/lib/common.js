e43c91f2ef69951922e408dba5aa8c0eb3fd5576
bluelovers <codelovers@users.sourceforge.net>
2017-05-22 17:14:09 +0800

    . (Step: 33)
    Step: 33
    
    [A ] new file:
    
    	dist/hosts/hosts.adaway.txt
    	dist/hosts/hosts.source.txt
    	dist/hosts/hosts.txt
    	script/hosts.gulp.task.js
    	src/hosts/00/keygen/BenVista.txt
    	"src/hosts/00/keygen/Process Lasso.txt"
    	src/hosts/00/keygen/adobe.txt
    	src/hosts/00/keygen/idm.txt
    	src/hosts/00/keygen/ultraedit.txt
    	src/hosts/iptable/ArenaNet.txt
    	src/hosts/iptable/appledaily.txt
    	src/hosts/iptable/baidu.txt
    	src/hosts/iptable/dmhy.txt
    	src/hosts/iptable/github.txt
    	src/hosts/iptable/iptable.txt
    	src/hosts/iptable/youtube.txt
    	src/leechblock/microsoft.txt
    	src/leechblock/utorrent.txt
    	src/site/content_farm/contw.com.scss
    	src/site/news/cnews.com.tw.scss
    
    [ M] modified - Changes not staged for commit:
    
    	dist/adaway.hosts.txt
    	dist/adblock.ini
    	dist/leechblock.list.txt
    	dist/stylish.adblock.mobile.css
    	dist/stylish.adblock.mobile.css.map
    	dist/stylish.facebook.css
    	dist/stylish.facebook.css.map
    	gulpfile.js
    	script/adaway.gulp.task.js
    	script/adblockpopups.gulp.task.js
    	script/leechblock.js
    	script/lib/adblock.js
    	script/lib/common.js
    	src/_include/_alldomain.scss
    	src/_include/_include_content_farm.scss
    	src/_include/_include_news.scss
    	src/_include_all.scss
    	src/_include_site.scss
    	src/lib/lib.scss
    	src/lib/var.stylish.facebook.scss
    	src/site/content_farm/bee101.com.scss
    	src/site/content_farm/bomb01.com.scss
    	src/site/content_farm/clickme.net.scss
    	src/site/content_farm/cmoney.tw.scss
    	src/site/content_farm/gjoyz.co.scss
    	src/site/content_farm/gleenow.com.scss
    	src/site/content_farm/ispot.news.scss
    	src/site/content_farm/life.tw.scss
    	src/site/content_farm/m.ipetgroup.com.scss
    	src/site/content_farm/pixpo.net.scss
    	src/site/content_farm/read01.com.scss
    	src/site/content_farm/teepr.com.scss
    	src/site/content_farm/twgreatdaily.com.scss
    	src/stylish.facebook.scss

--- script/lib/common.js
+++ script/lib/common.js
@@ -2,3 +2,67 @@
  * Created by user on 2017/5/8.
  */
 
+'use strict';
+
+const fs = require('fs');
+const globby = require('globby');
+const path = require('path');
+const Promise = require("bluebird");
+const LF = "\n";
+
+const self = {
+
+	fs: fs,
+	globby: globby,
+	path: path,
+	Promise: Promise,
+
+	LF: LF,
+
+	/**
+	 * @see http://www.jstips.co/zh_tw/javascript/deduplicate-an-array/
+	 */
+	array_unique(arr)
+	{
+		return Array.from(new Set(arr));
+	},
+
+};
+
+Object.assign(module.exports, self, {
+
+	readFileGlobby: async (a1, options = {}, ...args) =>
+	{
+		let data = await globby(a1, options).then(files => {
+			let source = '';
+
+			for (let file of files)
+			{
+				source += fs.readFileSync(options.cwd ? path.resolve(options.cwd, file) : file) + "\n";
+			}
+
+			return source;
+		});
+
+		return data;
+	},
+
+	readFileGlobbyList: async (a1, options = {}, ...args) =>
+	{
+		let data = await globby(a1, options).then(files => {
+			let source = {};
+
+			for (let file of files)
+			{
+				source[file] = fs.readFileSync(options.cwd ? path.resolve(options.cwd, file) : file) + "\n";
+			}
+
+			return source;
+		});
+
+		return data;
+	},
+
+});
+
+Object.assign(global, self);
