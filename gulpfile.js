1f2b25454048bc7366baa6430f75f3f8f177c5bb
bluelovers <codelovers@users.sourceforge.net>
2019-01-14 20:57:23 +0800

    .

--- gulpfile.js
+++ gulpfile.js
@@ -28,59 +28,63 @@ const options = {
 
 const scss_glob = './src/stylish.*.scss';
 
-gulp.task('sass', ['build-site'], function ()
+gulp.task('build-site', function (done)
+{
+	require('./script/build-site.js')(done);
+});
+
+gulp.task('sass', gulp.series('build-site', function ()
 {
 	return gulp.src(scss_glob)
 		.pipe(sourcemaps.init())
 			.pipe(sass(options).on('error', sass.logError))
 			.pipe(autoprefixer())
-		.pipe(sourcemaps.write('.', {
-			addComment: true
+		.pipe(sourcemaps.write({
+			addComment: true,
+			loadMaps: true,
+			includeContent: true,
 		}))
 	.pipe(gulp.dest('./dist'));
-});
+}));
 
 gulp.task('sass:watch', function ()
 {
 	gulp.watch(scss_glob, ['sass']);
 });
 
-gulp.task('build-site', function (done)
-{
-	require('./script/build-site.js')(done);
-});
+
 
 gulp.task('leechblock', function (done)
 {
 	require('./script/leechblock.js')(done);
 });
 
-gulp.task('adaway', ['leechblock'], function (done)
+gulp.task('adaway', gulp.series('leechblock', function (done)
 {
 	require('./script/adaway.gulp.task.js')(done);
-});
+}));
 
-gulp.task('noscript.untrusted', ['leechblock'], function (done)
+gulp.task('noscript.untrusted', gulp.series('leechblock', function (done)
 {
 	require('./script/noscript.untrusted.gulp.task.js')(done);
-});
+}));
 
-gulp.task('adblockpopups', ['leechblock'], function (done)
+gulp.task('adblockpopups', gulp.series('leechblock', function (done)
 {
 	require('./script/adblockpopups.gulp.task.js')(done);
-});
+}));
 
-gulp.task('adblock', ['leechblock', 'adblockpopups'], function (done)
+gulp.task('adblock', gulp.series('leechblock', 'adblockpopups', function (done)
 {
 	require('./script/adblock.gulp.task.js')(done);
-});
+}));
 
-gulp.task('hosts', ['adaway'], function (done)
+gulp.task('hosts', gulp.series('adaway', function (done)
 {
 	require('./script/hosts.gulp.task.js')(done);
-});
+}));
 
-gulp.task('all', [
+gulp.task('all', gulp.series(
 
 	'leechblock',
 
@@ -92,9 +96,9 @@ gulp.task('all', [
 
 	'hosts',
 
-	'sass',
+	'sass'
 
-], function (done)
+, function (done)
 {
 	done();
-});
+}));
